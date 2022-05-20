4e5e9256e74322104c901ee091f23fbef69357d2
AhmedMabrouk22 <105928025+AhmedMabrouk22@users.noreply.github.com>
2022-05-20 15:39:25 +0200

    Create B. Lucky Numbers (easy).cpp

---
+++ Codeforces/B. Lucky Numbers (easy).cpp
@@ -0,0 +1,59 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<bits/stdc++.h>
+#include<unordered_map>
+#define endl "\n"
+#define Ceil(x,y) ((x+y-1)/y)
+#define clr(arr, x) memset(arr, x, sizeof arr)
+#define all(v) v.begin(),v.end()
+#define allr(s) s.rbegin(),s.rend()
+#define rt(s) return cout<<s,0
+#define watch(x) cout<<(#x)<<" = "<<x<<endl
+#define sz(s)	(int)(s.size())
+#define full(v,n) for (int i = 0 ; i < n ; ++i) cin >> v[i]
+#define full1(v,n) for (int i = 1 ; i <= n ; ++i) cin >> v[i]
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef unsigned long long ull;
+void A_M_S()
+{
+#ifndef ONLINE_JUDGE
+	freopen("in.txt", "r", stdin);
+	freopen("out.txt", "w", stdout);
+#endif  !ONLINEJUDGE
+	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+}
+int dx[] { 1, -1, 0, 0, 1, 1, -1, -1 };
+int dy[] { 0, 0, 1, -1, 1, -1, 1, -1 };
+
+ll gcd(ll a, ll b) {
+	return b == 0 ? abs(a) : gcd(b, a % b);
+}
+ll lcm(ll a, ll b) {
+	return a / gcd(a, b) * b;
+}
+int n;
+ll solve(ll lucky, int fo, int se)
+{
+	if (lucky >= n && fo == se)
+		return lucky;
+
+	if (fo + se >= 10)
+		return 1e10;
+
+	return min(solve((lucky * 10) + 4, fo + 1, se)
+		, solve((lucky * 10) + 7, fo, se + 1));
+
+
+}
+
+int main()
+{
+	A_M_S();
+
+	cin >> n;
+
+	cout << solve(0, 0, 0);
+	return 0;
+}
