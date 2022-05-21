0e3bff70d817457dbfdc09ea228a90ff9982c6cd
Ahmed Mabrouk Said <105928025+AhmedMabrouk22@users.noreply.github.com>
2022-05-21 06:14:24 +0200

    Create BENEFACT - The Benefactor.cpp

---
+++ SPOJ/BENEFACT - The Benefactor.cpp
@@ -0,0 +1,87 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<bits/stdc++.h>
+#include<unordered_map>
+#define endl "\n"
+#define Ceil(x,y) ((x+y-1)/y)
+#define clr(arr, x) memset(arr, x, sizeof arr)
+#define all(v) v.begin(),v.end()
+#define allr(s) s.rbegin(),s.rend()
+#define rt(s) return cout<<s,0
+#define watch(x) cout<<(#x)<<" = "<<x<<endl
+#define sz(s)	(int)(s.size())
+#define full(v,n) for (int i = 0 ; i < n ; ++i) cin >> v[i]
+#define full1(v,n) for (int i = 1 ; i <= n ; ++i) cin >> v[i]
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef unsigned long long ull;
+void A_M_S()
+{
+#ifndef ONLINE_JUDGE
+	freopen("in.txt", "r", stdin);
+	freopen("out.txt", "w", stdout);
+#endif  !ONLINEJUDGE
+	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+}
+int dx[] { 1, -1, 0, 0, 1, 1, -1, -1 };
+int dy[] { 0, 0, 1, -1, 1, -1, 1, -1 };
+
+ll gcd(ll a, ll b) {
+	return b == 0 ? abs(a) : gcd(b, a % b);
+}
+ll lcm(ll a, ll b) {
+	return a / gcd(a, b) * b;
+}
+
+vector<vector<pair<int, int>>>graph;
+vector<bool>vis;
+ll mx, at;
+
+void dfs(ll u, ll cost)
+{
+	vis[u] = true;
+	if (cost > mx)
+	{
+		mx = cost;
+		at = u;
+	}
+
+	for (auto child : graph[u])
+	{
+		if (!vis[child.first])
+			dfs(child.first, cost + child.second);
+	}
+
+}
+
+int main()
+{
+	A_M_S();
+
+	int t;
+	cin >> t;
+	while (t--)
+	{
+		int n;
+		cin >> n;
+		mx = 0;
+		graph = vector<vector<pair<int, int>>>(n + 1);
+		vis = vector<bool>(n + 1);
+
+		for (int i = 0; i < n - 1; ++i)
+		{
+			int a, b, c;
+			cin >> a >> b >> c;
+			graph[a].push_back({ b, c });
+			graph[b].push_back({ a, c });
+		}
+
+		dfs(1, 0);
+		vis = vector<bool>(n + 1);
+		dfs(at, 0);
+		cout << mx << endl;
+	}
+
+	return 0;
+}
