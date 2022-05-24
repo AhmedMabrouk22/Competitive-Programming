a2c0d553f1c7d20d199d7896b0bca9061e418189
Ahmed Mabrouk Said <105928025+AhmedMabrouk22@users.noreply.github.com>
2022-05-24 09:32:20 +0200

    Create E. Cyclic Components.cpp

---
+++ Codeforces/Div3/E. Cyclic Components.cpp
@@ -0,0 +1,89 @@
+// problem link: https://codeforces.com/problemset/problem/977/E
+#define _CRT_SECURE_NO_WARNINGS
+#include<bits/stdc++.h>
+#include<unordered_map>
+#define endl "\n"
+#define Ceil(x,y) ((x+y-1)/y)
+#define clr(arr, x) memset(arr, x, sizeof arr)
+#define all(v) v.begin(),v.end()
+#define allr(s) s.rbegin(),s.rend()
+#define rt(s) return cout<<s,0
+#define watch(x) cout<<(#x)<<" = "<<x<<endl
+#define sz(s)	(int)(s.size())
+#define full(v,n) for (int i = 0 ; i < n ; ++i) cin >> v[i]
+#define full1(v,n) for (int i = 1 ; i <= n ; ++i) cin >> v[i]
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef unsigned long long ull;
+void A_M_S()
+{
+#ifndef ONLINE_JUDGE
+	freopen("in.txt", "r", stdin);
+	freopen("out.txt", "w", stdout);
+#endif  !ONLINEJUDGE
+	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+}
+int dx[] { 1, -1, 0, 0, 1, 1, -1, -1 };
+int dy[] { 0, 0, 1, -1, 1, -1, 1, -1 };
+
+ll gcd(ll a, ll b) {
+	return b == 0 ? abs(a) : gcd(b, a % b);
+}
+ll lcm(ll a, ll b) {
+	return a / gcd(a, b) * b;
+}
+
+vector<vector<int>>graph;
+vector<bool>vis;
+vector<int>nodes , ed;
+bool cy;
+void dfs(int i, int par)
+{
+	vis[i] = true;
+	nodes.push_back(i);
+	for (auto child : graph[i])
+	{
+		if (!vis[child])
+			dfs(child, i);
+		else if (child != par)
+			cy = true;
+	}
+}
+
+
+int main()
+{
+	A_M_S();
+
+	int n, m;
+	cin >> n >> m;
+	graph = vector<vector<int>>(n + 1);
+	vis = vector<bool>(n + 1);
+	ed = vector<int>(n + 1);
+	for (int i = 0; i < m; ++i)
+	{
+		int a, b;
+		cin >> a >> b;
+		graph[a].push_back(b);
+		graph[b].push_back(a);
+		++ed[a];
+		++ed[b];
+	}
+	int res = 0;
+	for (int i = 1; i <= n; ++i)
+	{
+		if (!vis[i])
+		{
+			nodes.clear();
+			cy = 0;
+			dfs(i, -1);
+			for (auto x : nodes)
+				cy &= (ed[x] == 2);
+			res += cy;
+		}
+	}
+	cout << res << endl;
+	return 0;
+}
