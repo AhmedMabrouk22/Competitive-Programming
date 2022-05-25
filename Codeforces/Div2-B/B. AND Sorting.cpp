69474be3b8176103d7a1e5ed420a4961b545ce8b
Ahmed Mabrouk Said <105928025+AhmedMabrouk22@users.noreply.github.com>
2022-05-25 11:39:44 +0200

    Create B. AND Sorting.cpp

---
+++ Codeforces/Div2-B/B. AND Sorting.cpp
@@ -0,0 +1,65 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<bits/stdc++.h>
+#include<unordered_map>
+#define endl "\n"
+#define Ceil(x,y) ((x+y-1)/y)
+#define clr(arr, x) memset(arr, x, sizeof arr)
+#define all(v) v.begin(),v.end()
+#define allr(s) s.rbegin(),s.rend()
+#define rt(s) return cout<<s,0
+#define watch(x) cout<<(#x)<<" = "<<x<<endl
+#define sz(s)	(int)(s.size())
+#define full(v,n) for (int i = 0 ; i < n ; ++i) cin >> v[i]
+#define full1(v,n) for (int i = 1 ; i <= n ; ++i) cin >> v[i]
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef unsigned long long ull;
+void A_M_S()
+{
+#ifndef ONLINE_JUDGE
+	freopen("in.txt", "r", stdin);
+	freopen("out.txt", "w", stdout);
+#endif  !ONLINEJUDGE
+	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+}
+int dx[] { 1, -1, 0, 0, 1, 1, -1, -1 };
+int dy[] { 0, 0, 1, -1, 1, -1, 1, -1 };
+
+ll gcd(ll a, ll b) {
+	return b == 0 ? abs(a) : gcd(b, a % b);
+}
+ll lcm(ll a, ll b) {
+	return a / gcd(a, b) * b;
+}
+int main()
+{
+	A_M_S();
+
+	int t;
+	cin >> t;
+	while (t--)
+	{
+		int n;
+		cin >> n;
+		vector<int>v(n);
+		full(v, n);
+		int ans;
+		for (int i = 0; i < n; ++i)
+		{
+			if (i != v[i])
+				ans = v[i];
+		}
+
+		for (int i = 0; i < n; ++i)
+		{
+			if (i != v[i])
+				ans &= v[i];
+		}
+		cout << ans << endl;
+		
+	}
+
+	return 0;
+}
