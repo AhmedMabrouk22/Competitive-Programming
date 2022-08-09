829fb808efd19db5ef0e8b905344fdb069f6c69e
Ahmed Mabrouk Said <105928025+AhmedMabrouk22@users.noreply.github.com>
2022-08-09 08:11:56 +0200

    Create C. Palindrome Transformation.cpp

---
+++ Codeforces/Div2-C/C. Palindrome Transformation.cpp
@@ -0,0 +1,67 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<bits/stdc++.h>
+#include<unordered_map>
+#define endl "\n"
+#define Ceil(x,y) ((x+y-1)/y)
+#define clr(arr, x) memset(arr, x, sizeof arr)
+#define all(v) v.begin(),v.end()
+#define allr(s) s.rbegin(),s.rend()
+#define rt(s) return cout<<s,0
+#define watch(x) cout<<(#x)<<" = "<<x<<endl
+#define sz(s)	(int)(s.size())
+#define full(v,n) for (int i = 0 ; i < n ; ++i) cin >> v[i]
+#define full1(v,n) for (int i = 1 ; i <= n ; ++i) cin >> v[i]
+#define OO 0x3f3f3f3f3f3f3f3fLL
+using namespace std;
+const int oo = 0x3f3f3f3f;
+typedef long long ll;
+typedef long double ld;
+typedef unsigned long long ull;
+void A_M_S()
+{
+#ifndef ONLINE_JUDGE
+	freopen("in.txt", "r", stdin);
+	freopen("out.txt", "w", stdout);
+#endif  !ONLINEJUDGE
+	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+}
+int dx[] { 1, -1, 0, 0, 1, 1, -1, -1 };
+int dy[] { 0, 0, 1, -1, 1, -1, 1, -1 };
+
+ll gcd(ll a, ll b) {
+	return b == 0 ? abs(a) : gcd(b, a % b);
+}
+ll lcm(ll a, ll b) {
+	return a / gcd(a, b) * b;
+}
+
+int main()
+{
+	A_M_S();
+	int n, p;
+	string s;
+	cin >> n >> p >> s;
+	--n;
+	--p;
+	if (p > n / 2)
+		p = n - p;
+
+	int l = -1, r = 0, res = 0;
+	for (int i = 0; i <= n / 2; ++i)
+	{
+		if (s[i] != s[n - i])
+		{
+			int ud = abs(s[i] - s[n - i]);
+			res += min(ud, 26 - ud);
+			if (l < 0)
+				l = i;
+			else
+				r = i;
+		}
+	}
+
+	res += min(abs(p - l), abs(p - r));
+	res += max(0, r - l);
+	cout << (l < 0 ? 0 : res);
+	return 0;
+}
