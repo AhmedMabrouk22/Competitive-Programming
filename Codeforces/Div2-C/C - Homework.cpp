3a5bb5c5bad3ba2581b4a46fc0b3eb678b46db8f
Ahmed Mabrouk Said <105928025+AhmedMabrouk22@users.noreply.github.com>
2022-08-13 09:15:57 +0200

    Create C - Homework.cpp

---
+++ Codeforces/Div2-C/C - Homework.cpp
@@ -0,0 +1,75 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<bits/stdc++.h>
+#include<unordered_map>
+#define endl "\n"
+#define Ceil(x,y) ((x+y-1)/y)
+#define clr(arr, x) memset(arr, x, sizeof arr)
+#define all(v) v.begin(),v.end()
+#define allr(s) s.rbegin(),s.rend()
+#define rt(s) return cout<<s,0
+#define watch(x) cout<<(#x)<<" = "<<x<<endl
+#define sz(s)	(int)(s.size())
+#define full(v,n) for (int i = 0 ; i < n ; ++i) cin >> v[i]
+#define full1(v,n) for (int i = 1 ; i <= n ; ++i) cin >> v[i]
+#define OO 0x3f3f3f3f3f3f3f3fLL
+using namespace std;
+const int oo = 0x3f3f3f3f;
+typedef long long ll;
+typedef long double ld;
+typedef unsigned long long ull;
+void A_M_S()
+{
+#ifndef ONLINE_JUDGE
+	freopen("in.txt", "r", stdin);
+	freopen("out.txt", "w", stdout);
+#endif  !ONLINEJUDGE
+	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+}
+int dx[] { 1, -1, 0, 0, 1, 1, -1, -1 };
+int dy[] { 0, 0, 1, -1, 1, -1, 1, -1 };
+
+ll gcd(ll a, ll b) {
+	return b == 0 ? abs(a) : gcd(b, a % b);
+}
+ll lcm(ll a, ll b) {
+	return a / gcd(a, b) * b;
+}
+
+int main()
+{
+	A_M_S();
+	string s;
+	int k;
+	cin >> s >> k;
+	int frq[26] = { 0 };
+	vector <pair<int, char>>v;
+	for (auto i : s)
+		++frq[i - 'a'];
+	for (int i = 0; i < 26; ++i)
+	{
+		if (frq[i])
+			v.push_back({ frq[i], i + 'a' });
+	}
+	sort(all(v));
+	int c = 0;
+	for (auto &i : v)
+	{
+		if (i.first <= k)
+		{
+			k -= i.first;
+			i.first = 0;
+			frq[i.second - 'a'] = 0;
+			++c;
+		}
+		else
+			break;
+	}
+
+	cout << v.size() - c << endl;
+	for (auto i : s)
+	{
+		if (frq[i - 'a'])
+			cout << i;
+	}
+	return 0;
+}
