dfba62967e81bc6d25ef9241e01f50bb281b8014
Ahmed Mabrouk Said <105928025+AhmedMabrouk22@users.noreply.github.com>
2022-09-01 07:09:03 +0200

    Create C - Vacation.cpp

---
+++ AtCoder - Educational DP Contest/C - Vacation.cpp
@@ -0,0 +1,73 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<bits/stdc++.h>
+#include<unordered_map>
+#define endl "\n"
+#define Ceil(x,y) ((x+y-1)/y)
+#define clr(arr, x) memset(arr, x, sizeof arr)
+#define all(v) v.begin(),v.end()
+#define allr(s) s.rbegin(),s.rend()
+#define rt(s) return cout<<s,0
+#define watch(x) cout<<(#x)<<" = "<<x<<endl
+#define sz(s)	(int)(s.size())
+#define full(v,n) for (int i = 0 ; i < n ; ++i) cin >> v[i]
+#define full1(v,n) for (int i = 1 ; i <= n ; ++i) cin >> v[i]
+#define OO 0x3f3f3f3f3f3f3f3fLL
+using namespace std;
+const int oo = 0x3f3f3f3f;
+typedef long long ll;
+typedef long double ld;
+typedef unsigned long long ull;
+void A_M_S()
+{
+#ifndef ONLINE_JUDGE
+	freopen("in.txt", "r", stdin);
+	freopen("out.txt", "w", stdout);
+#endif  !ONLINEJUDGE
+	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+}
+int dx[] { 1, -1, 0, 0, 1, 1, -1, -1 };
+int dy[] { 0, 0, 1, -1, 1, -1, 1, -1 };
+
+ll gcd(ll a, ll b) {
+	return b == 0 ? abs(a) : gcd(b, a % b);
+}
+ll lcm(ll a, ll b) {
+	return a / gcd(a, b) * b;
+}
+const int MAX_IN = 1e5 + 5;
+int n;
+ll memo[MAX_IN][4];
+int arr[MAX_IN][4];
+ll solve(int i, int t)
+{
+	if (i == n)
+		return 0;
+	if (memo[i][t] != -1)
+		return memo[i][t];
+
+	ll c1 = 0, c2 = 0, c3 = 0;
+	if (t == 0)
+		c2 = max(solve(i + 1, 1) + arr[i][1], solve(i + 1, 2) + arr[i][2]);
+	else if (t == 1)
+		c2 = max(solve(i + 1, 0) + arr[i][0], solve(i + 1, 2) + arr[i][2]);
+	else if (t == 2)
+		c3 = max(solve(i + 1, 1) + arr[i][1], solve(i + 1, 0) + arr[i][0]);
+	
+	return memo[i][t] = max(c1, max(c2, c3));
+}
+int main()
+{
+	A_M_S();
+	clr(memo, -1);
+	cin >> n;
+	for (int i = 0; i < n; ++i)
+	{
+		for (int x = 0; x < 3; ++x)
+			cin >> arr[i][x];
+	}
+	ll res = 0;
+	for (int i = 0; i < 3; ++i)
+		res = max(res, solve(0, i));
+	cout << res;
+	return 0;
+}
