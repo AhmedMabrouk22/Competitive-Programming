9b401ce03032fe785686538e4a5823afefa3b4f6
Ahmed Mabrouk Said <105928025+AhmedMabrouk22@users.noreply.github.com>
2022-10-29 22:02:02 +0200

    Create G - Sum of product of pairs.cpp

---
+++ AtCoder/G - Sum of product of pairs.cpp
@@ -0,0 +1,62 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<bits/stdc++.h>
+#include<unordered_map>
+#define endl "\n"
+#define Ceil(x,y) ((x+y-1)/y)
+#define clr(arr, x) memset(arr, x, sizeof arr)
+#define all(v) v.begin(),v.end()
+#define allr(s) s.rbegin(),s.rend()
+#define rt(s) return cout<<s,0
+#define watch(x) cout<<(#x)<<" = "<<x<<endl
+#define sz(s)	(int)(s.size())
+#define full(v,n) for (int i = 0 ; i < n ; ++i) cin >> v[i]
+#define full1(v,n) for (int i = 1 ; i <= n ; ++i) cin >> v[i]
+#define OO 0x3f3f3f3f3f3f3f3fLL
+using namespace std;
+const int oo = 0x3f3f3f3f;
+typedef long long ll;
+typedef long double ld;
+typedef unsigned long long ull;
+void A_M_S()
+{
+#ifndef ONLINE_JUDGE
+	freopen("in.txt", "r", stdin);
+	freopen("out.txt", "w", stdout);
+#endif  !ONLINEJUDGE
+	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+}
+int dx[] { 1, -1, 0, 0, 1, 1, -1, -1 };
+int dy[] { 0, 0, 1, -1, 1, -1, 1, -1 };
+
+ll gcd(ll a, ll b) {
+	return b == 0 ? abs(a) : gcd(b, a % b);
+}
+ll lcm(ll a, ll b) {
+	return a / gcd(a, b) * b;
+}
+const int MAX_IN = 1e5 + 5;
+int MOD = 1e9 + 7;
+int main()
+{
+	A_M_S();
+	int T = 1;
+	//cin >> T;
+	while (T--)
+	{
+		int n;
+		cin >> n;
+		vector<ll>v(n+1);
+		full(v, n);
+		vector<ll>prf(n + 1);
+
+		prf[n - 1] = v[n - 1];
+		for (int i = n - 2; i >= 0; --i)
+			prf[i] = (prf[i + 1] + v[i]) % MOD;
+
+		ll res = 0;
+		for (int i = 0; i < n; ++i)
+			res = ((res % MOD) + ((prf[i+1]*v[i]) % MOD)) % MOD;
+		cout << res;
+	}
+	return 0;
+}
